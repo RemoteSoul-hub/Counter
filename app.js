476b5c880ff0f50bdd0fabc09ea371e450c96fa2
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-02-28 23:49:13 +0100

    app.js
    app.js, one bug

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-//
+
 let count = 0;
 
 let value = document.getElementById('value');
-let increase = document.querySelector('increase');
+let increase = document.querySelector('btn increase');
 let decrease = document.querySelector('decrease');
 let reset = document.querySelector('reset');
+function addValue() {
+    console.log(2);
+}
 
-document.addEventListener('click', )
+increase.addEventListener('click', addValue);
+
