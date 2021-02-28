0027dc14491a976a26ae3b3248b2d50f101d31a1
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-02-28 23:27:10 +0100

    2nd commit
    Base structure, start of script writing

---
+++ app.js
@@ -0,0 +1,9 @@
+//
+let count = 0;
+
+let value = document.getElementById('value');
+let increase = document.querySelector('increase');
+let decrease = document.querySelector('decrease');
+let reset = document.querySelector('reset');
+
+document.addEventListener('click', )
