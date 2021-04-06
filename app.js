78cc63bd8f9d59192a84c6b065e4d5f099ab7a5f
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-04-06 17:41:34 +0100

    quick fixes

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-
+// set inital value to zero
 let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-let value = document.getElementById('value');
-let increase = document.querySelector('btn increase');
-let decrease = document.querySelector('decrease');
-let reset = document.querySelector('reset');
-function addValue() {
-    console.log(2);
-}
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
 
-increase.addEventListener('click', addValue);
-
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
